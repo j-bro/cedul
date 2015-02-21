c01705122b11a6433dcdee86151ed501b3b5cc30
Jeremy Brown <jj.brown@hotmail.com>
2015-02-21 14:14:50 -0500

    Tweaked model admin. Removed 'none' default warnings.

--- cedul_app/admin.py
+++ cedul_app/admin.py
@@ -2,7 +2,20 @@ from django.contrib import admin
 
 from cedul_app.models import Event, Attendee, EventTime
 
+
+class AttendeeInline(admin.TabularInline):
+	model = Attendee
+	extra = 3
+
+class EventTimeInline(admin.TabularInline):
+	model = EventTime
+	extra = 3
+
+class EventAdmin(admin.ModelAdmin):
+	inlines = [AttendeeInline, EventTimeInline]
+	list_display = ('event_name', 'location', 'description')
+
 # Register your models here.
-admin.site.register(Event)
+admin.site.register(Event, EventAdmin)
 admin.site.register(Attendee)
 admin.site.register(EventTime)
--- cedul_app/models.py
+++ cedul_app/models.py
@@ -7,11 +7,11 @@ class Event(models.Model):
 	event_name = models.CharField(max_length=200, blank=False)
 	location = models.CharField(max_length=200, blank=True)
 	description = models.TextField(blank=True)
-	recurring = models.BooleanField(blank=False)
-	require_all = models.BooleanField(blank=False)
+	recurring = models.BooleanField(default=False, blank=False)
+	require_all = models.BooleanField(default=False, blank=False)
 
 	def __str__(self):
-		return self.event_name + 'at' + self.location
+		return self.event_name
 
 
 class Attendee(models.Model):
@@ -20,6 +20,12 @@ class Attendee(models.Model):
 	last_name = models.CharField(max_length=200, blank=False)
 	email = models.EmailField(blank=True)
 
+	def __str__(self):
+		return self.first_name + ' ' + self.last_name
+
 class EventTime(models.Model):
 	event = models.ForeignKey(Event, blank=False)
 	time = models.DateTimeField(blank=False)
+
+	def __str__(self):
+		return str(self.time)
